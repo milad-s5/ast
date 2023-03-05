253a2131162998b885fef744dc40150d87face92
milad-s5 <miladsamimifar2013@gmail.com>
2023-03-05 19:11:00 +0330

    .

--- Installation.sh
+++ Installation.sh
@@ -4,9 +4,8 @@ source venv/bin/activate
 which python3.8
 python3.8 -V
 pip install --upgrade pip
-pip list
-# deactivate
 # pip freeze > requirements.txt
 pip --default-timeout=100 install -r ./requirements.txt 
 curl -C - -L -o './pretrained_models/audio_mdl.pth' 'https://www.dropbox.com/s/cv4knew8mvbrnvq/audioset_0.4593.pth?dl=1'
 python ./AST_Inference_Demo_CPU.py
+deactivate
